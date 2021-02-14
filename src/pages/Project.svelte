052af2ec97a4481defffa294534d9d69176ee70e
SB <sul4bh@gmail.com>
2021-02-14 13:48:13 -0800

    updates pages

--- src/pages/Project.svelte
+++ src/pages/Project.svelte
@@ -5,27 +5,27 @@
 </style>
 
 <h2>Ongoing</h2>
-<div class="ml-2 .list-disc">
-  <p class="description">
+<ul class="ml-8 list-disc list-outside">
+  <li class="description">
     Developing multi-GPA-Tree, a statistical approach to prioritize GWAS results
     and identify combinations of functional annotations by leveraging pleiotropy
     and integrating genomic functional annotations.
-  </p>
-  <p class="description">
+  </li>
+  <li class="description">
     Exploring correlations between nailfold capillary parameters and myositis
     specific antibodies in newly diagnosed juvenile dermatomyositis patients
     (retrospective cross-sectional cohort).
-  </p>
-  <p class="description">
+  </li>
+  <li class="description">
     Identifying factors associated with retention of patient for Hepatitis C
     treatment (cross-sectional study).
-  </p>
-  <p class="description">
+  </li>
+  <li class="description">
     Predicting renal flare in LN patients using select urine and serum
     biomarkers (longitudinal cohort).
-  </p>
-  <p class="description">
+  </li>
+  <li class="description">
     Establishing genetic predisposition profiles of Lupus Nephritis patients to
     identify those at risk for renal damage (longitudinal cohort).
-  </p>
-</div>
+  </li>
+</ul>
