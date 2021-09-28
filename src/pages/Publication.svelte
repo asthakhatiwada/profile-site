226042406278a1bbc02c9716967896233062f019
Aastha Khatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2021-09-28 14:31:02 -0600

    Update Publication.svelte

--- src/pages/Publication.svelte
+++ src/pages/Publication.svelte
@@ -128,8 +128,8 @@ https://github.com/svelte-add/tailwindcss/issues/8
       Song NJ, Allen C, Vilgelm AE, Riesenberg BP, Weller KP, Reynolds K, 
       Chakravarthy KB, Kumar A, <strong>Khatiwada A</strong>, Sun Z, Ma A, Chang Y, Yusuf M,Li A, 
       Zeng C, Evans JP, Bucci D, Gunasena M, Xu M, Liyanage NPM, Bolyard C, 
-      Velegraki M, Liu SL, Ma Q, Devenport M, Liu Y, Zheng P, Malvestutto CD#, 
-      Chung D#, Li Z#. Immunological insights into the therapeutic roles of 
+      Velegraki M, Liu SL, Ma Q, Devenport M, Liu Y, Zheng P, Malvestutto CD, 
+      Chung D, Li Z. Immunological insights into the therapeutic roles of 
       soluble CD24 against severe COVID-19. Submitted to
       <span class="italic">New England Journal of Medicine.</span>
     </p>
