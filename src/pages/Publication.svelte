aa73006fc3cc5b697766e3e64824377dec3a6a0d
Aastha Khatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2021-11-29 13:00:04 -0700

    Update Publication.svelte

--- src/pages/Publication.svelte
+++ src/pages/Publication.svelte
@@ -30,7 +30,9 @@ https://github.com/svelte-add/tailwindcss/issues/8
 <div>
   <div class="publication-item md:flex">
     <div class="image-wrapper">
+      <img class="publication-image" src="/public/img/pr.png" />
     </div>
+    <div>
     <p>
       <strong>Khatiwada A</strong>, Wolf BJ, Yilmaz AS, Ramos PS, Pietrzak M, Lawson A, Hunt KJ, Kim HJ, Chung D.
       <a
