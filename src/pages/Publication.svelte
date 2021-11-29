acdc5765bc8ac66d77370fad9997d653ee6ed209
Aastha Khatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2021-11-29 12:41:39 -0700

    Update Publication.svelte

--- src/pages/Publication.svelte
+++ src/pages/Publication.svelte
@@ -30,7 +30,7 @@ https://github.com/svelte-add/tailwindcss/issues/8
 <div>
   <div class="publication-item md:flex">
     <div class="image-wrapper">
-      <img class="publication-image" src="/public/img/po.gif" />
+      <img class="publication-image" src="/public/img/Bioinformatics_journal_cover.gif" />
     </div>
     <p>
       <strong>Khatiwada A</strong>, Wolf BJ, Yilmaz AS, Ramos PS, Pietrzak M, Lawson A, Hunt KJ, Kim HJ, Chung D.
