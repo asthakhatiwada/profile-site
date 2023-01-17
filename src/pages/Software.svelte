475c07c44f0acdd9aa87319e4de0eb42f44b7524
Aastha Khatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2023-01-17 15:21:48 -0700

    Update Software.svelte

--- src/pages/Software.svelte
+++ src/pages/Software.svelte
@@ -18,5 +18,22 @@
         <a class="external-link" href="https://github.com/asthakhatiwada/GPATree">Github</a>.
       </p>
     </div>
+    
+    <div class="pr-4">
+      <img class="m-auto" style="width: 150px" src="/public/img/gpatree.png" />
+    </div>
+    <div class="flex-1">
+      <h2 class="mt-0">GPATree</h2>
+      <p>R package to implement the GPA-Tree method.</p>
+      <p>
+        R Shiny app to interactively implement association mapping and investigate
+        functional annotation tree.
+      </p>
+      <p>
+        Download from
+        <a class="external-link" href="https://github.com/asthakhatiwada/GPATree">Github</a>.
+      </p>
+    </div>
+    
   </div>
 </div>
