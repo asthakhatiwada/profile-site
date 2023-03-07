a2c618218736d33db8c720a39fb5dde6f019076b
Aastha Khatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2023-03-06 22:31:36 -0700

    Update Software.svelte

--- src/pages/Software.svelte
+++ src/pages/Software.svelte
@@ -26,18 +26,31 @@
     <div class="px-2">
       <div class="block md:flex items-center">
         <div class="pr-4">
-          <img class="m-auto" style="width: 150px" src="/public/img/gpatree.png" />
+          <img class="m-auto" style="width: 150px" src="/public/img/multigpatree.png" />
         </div>
         <div class="flex-1">
-          <h2 class="mt-0">GPATree</h2>
-          <p>R package to implement the GPA-Tree method.</p>
-          <p>
-            R Shiny app to interactively implement association mapping and investigate
-        functional annotation tree.
-          </p>
+          <h2 class="mt-0">multiGPATree</h2>
+          <p>R package to implement the multi-GPA-Tree method.</p>
           <p>
             Download from
-            <a class="external-link" href="https://github.com/asthakhatiwada/GPATree">Github</a>.
+            <a class="external-link" href="https://github.com/asthakhatiwada/multiGPATree">Github</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+
+<div class="px-2">
+      <div class="block md:flex items-center">
+        <div class="pr-4">
+          <img class="m-auto" style="width: 150px" src="/public/img/trackCOPD.png" />
+        </div>
+        <div class="flex-1">
+          <h2 class="mt-0">trackCOPDPro</h2>
+          <p>R Shiny app to interactively implement linear mixed models to jointly model changes in FEV1 and FEV1/FVC, 
+            using important baseline predictors to better understand the factors that affect progression.</p>
+          <p>
+            Access link
+            <a class="external-link" href="https://aasthakhatiwada.shinyapps.io/software/">Github</a>.
           </p>
         </div>
       </div>
