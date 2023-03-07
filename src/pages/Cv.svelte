ab50ceea78d01bc837d6a70ce797ae6e3af6ab13
Aastha Khatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2023-03-06 22:39:01 -0700

    Update Cv.svelte

--- src/pages/Cv.svelte
+++ src/pages/Cv.svelte
@@ -1,5 +1,5 @@
 <div class="mb-4">
-  <a class="external-link" href="/public/misc/Khatiwada_CV_updated03-06-2023.pdf" download>
+  <a class="external-link" href="/public/misc/Khatiwada_CV_updated03062023.pdf" download>
     Download CV
   </a>
 </div>
